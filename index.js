03361c015a1e90f132eca1f33f48e5d9399d1f78
Artem Kurchenko <meschken@yandex.ru>
2022-05-18 23:46:19 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const tasks = [
+  'Сделать проектную работу',
+  'Полить цветы',
+  // `<img src="somelink" onerror="alert('вас взломали')" />`,
+  'Пройти туториал по Реакту',
+  'Сделать фронт для своего проекта',
+  'Погулять с собакой',
+  'Разобраться в замыканиях',
+  'Решить задачу на Codewars',
+];
+
+const todosListElement = document.querySelector('.todos__list');
+const todosFormElement = document.querySelector('.todos__form');
+const todosTemplateElement = document.querySelector('.todo-template');
+const todosInputElement = document.querySelector('.todos__input');
