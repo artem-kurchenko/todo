18aa6b32d707321c65ffde6186c977776440598e
Artem Kurchenko <meschken@yandex.ru>
2022-05-19 20:23:55 +0400

    initial rendering

--- index.js
+++ index.js
@@ -13,3 +13,26 @@ const todosListElement = document.querySelector('.todos__list');
 const todosFormElement = document.querySelector('.todos__form');
 const todosTemplateElement = document.querySelector('.todo-template');
 const todosInputElement = document.querySelector('.todos__input');
+
+function renderList(data){
+     data.forEach(item=>renderItem(item))
+}
+function renderItem(text){
+    let template = `<li class="todo">
+    <p class="todo__text">
+      ${text}
+    </p>
+    <button class="button todo__btn todo__btn_type_edit">
+
+    </button>
+    <button class="todo__btn button todo__btn_type_copy">
+
+    </button>
+    <button class="todo__btn button todo__btn_type_remove">
+
+    </button>
+  </li>`
+  todosListElement.insertAdjacentHTML('beforeend', template);
+}
+renderList(tasks);
+
